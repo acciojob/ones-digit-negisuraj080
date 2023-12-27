21abb0df1fec0e564b59964cd9e51f7dca0069e1
negisuraj080 <143009358+negisuraj080@users.noreply.github.com>
2023-12-27 21:23:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function onesDigit(n) {
   // your code here
+	// Check if the input is a positive integer
+    if (!Number.isInteger(number) || number <= 0) {
+        throw new Error("Input must be a positive integer");
+    }
+
+    // Convert the number to a string to handle leading zeros
+    const numberString = number.toString();
+
+    // Get the ones digit
+    const onesDigit = parseInt(numberString.charAt(numberString.length - 1));
+
+    return onesDigit;
 }
 
 // Do not change the code below
